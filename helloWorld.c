d594b6ae03e1490e514f2eec748f9c9639965ec1
D4rkHeart <Leonardo.surdez@gmail.com>
2022-08-31 15:20:20 +0000

    [feat] Hello world + gitignore

---
+++ helloWorld.c
@@ -0,0 +1,12 @@
+//The programms display a hello world in the console 
+//V1.1.2
+//
+//to build :
+//gcc -Wall helloWorld.c -o ./compiledFiles/helloWorld
+
+#include <stdio.h>
+
+int main (){
+    printf("\nHello world 17:11:16 \n\n");
+    return 0;
+}
